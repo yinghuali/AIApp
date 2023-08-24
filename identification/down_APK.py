d3427db2a81fc9cf3ec3ae6dfd8140f9ad18d07c
Yinghua Li <yinghua.li@uni.lu>
2023-08-25 00:04:12 +0200

    Update

--- identification/down_APK.py
+++ identification/down_APK.py
@@ -13,7 +13,7 @@ def down_apk(path_sha256_pkl):
     sha256_list = get_sha256(path_sha256_pkl)
     i = 0
     for sha256_str in sha256_list:
-        down_command = 'curl -O --remote-header-name -G -d apikey=8c08e1e623110c600186098a11ba882a7e323ad32b71868510c971a504eca3f9 -d sha256={sha256} https://androzoo.uni.lu/api/download'
+        down_command = 'curl -O --remote-header-name -G -d apikey=*** -d sha256={sha256} https://androzoo.uni.lu/api/download'
         down_command = down_command.format(sha256=sha256_str)
         os.system(down_command)
         i += 1
